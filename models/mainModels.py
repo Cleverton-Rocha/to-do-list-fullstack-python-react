8cb528cc91816a013ce025cf504565a6d0b45bb6
ultr4 <cl.rch09@icloud.com>
2023-10-18 12:40:07 -0300

    initial commit

---
+++ models/mainModels.py
@@ -0,0 +1,84 @@
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.orm import sessionmaker, declarative_base
+from datetime import datetime
+
+date = datetime.now()
+
+db_user = "postgres"
+db_password = "1902"
+db_host = "localhost"
+db_port = "5432"
+db_name = "to_do_list"
+
+
+db_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+engine = create_engine(db_url)
+
+Base = declarative_base()
+
+
+class Tarefas(Base):
+    __tablename__ = 'tarefas'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    title = Column(String(255), nullable=False, unique=True)
+    status = Column(String, default='pending', nullable=False)
+    created_at = Column(Date, default=date)
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def create_task(title):
+    data = Tarefas(title=title)
+    session.add(data)
+    session.commit()
+    session.close()
+
+
+def delete_task(task_id):
+
+    task = session.query(Tarefas).filter_by(id=task_id).first()
+
+    if task:
+        session.delete(task)
+        session.commit()
+        print(f"Task com ID {task_id} foi excluida")
+    else:
+        print(f"Tarefa com ID {task_id} não encontrada.")
+
+
+def get_task():
+    tasks = session.query(Tarefas).all()
+    task_list = []
+
+    for task in tasks:
+        task_dict = {
+            "id": task.id,
+            "title": task.title,
+            "status": task.status,
+            "created_at": task.created_at.strftime("%Y-%m-%d")
+        }
+        task_list.append(task_dict)
+
+    return task_list
+
+
+def update_task(task_id, new_title=None, new_status=None):
+    task = session.query(Tarefas).filter_by(id=task_id).first()
+
+    if task:
+        if new_title is not None:
+            task.title = new_title
+        if new_status is not None:
+            task.status = new_status
+
+        session.commit()
+        print(f"Tarefa com ID {task_id} foi atualizada")
+        return "success"
+    else:
+        print(f"Tarefa com ID {task_id} não encontrada.")
+        return "not_found"
---
+++ router.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request
+from models import mainModels
+
+app = Flask(__name__)
+
+
+@app.route('/get', methods=['GET'])
+def get():
+    if request.method == 'GET':
+        all_tasks = mainModels.get_task()
+        return jsonify(all_tasks)
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if 'title' in data:
+            title = data['title']
+            mainModels.create_task(title)
+
+            return jsonify({'message': 'Tarefa criada com sucesso'}), 201
+        else:
+
+            return jsonify({'error': 'Campos obrigatórios ausentes'}), 400
+
+    return jsonify({'error': 'Método não suportado'}), 405
+
+
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    if request.method == 'DELETE':
+        data = request.get_json()
+
+        if 'id' in data:
+            id = data['id']
+            mainModels.delete_task(id)
+
+            return jsonify({'message': 'Tarefa deletada com sucesso'}), 201
+        else:
+            return jsonify({'message': 'Tarefa não encontrada'}), 400
+
+
+@app.route('/update/<int:task_id>', methods=['PUT'])
+def update(task_id):
+    if request.method == 'PUT':
+        new_title = request.json.get('new_title')
+        new_status = request.json.get('new_status')
+
+        if new_title is None and new_status is None:
+            return jsonify({'message': 'Nenhum dado de atualização fornecido'}), 400
+
+        result = mainModels.update_task(task_id, new_title, new_status)
+
+        if result == "not_found":
+            return jsonify({'message': f'Tarefa com ID {task_id} não encontrada.'}), 404
+        elif result == "success":
+            return jsonify({'message': f'Tarefa com ID {task_id} foi atualizada'}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
